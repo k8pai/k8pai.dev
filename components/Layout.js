ba51be879ca8791a96aa2603580a5d0099171d04
k8pai <thek8pai@gmail.com>
2023-01-17 03:06:12 +0530

    committ with certification updation and layout code comments

--- components/Layout.js
+++ components/Layout.js
@@ -5,29 +5,26 @@ import Header from './Header'
 
 const Layout = ({children}) => {
 	
-	let lastScroll = 0;
-	addEventListener("load", () => {
-		console.log('loaded');
-	});
-	window.addEventListener("scroll", () => {
-		const cont = document.querySelector(".layoutsection");
-		const currentScroll = window.pageYOffset;
+	// let lastScroll = 0;
+	// window.addEventListener("scroll", () => {
+	// 	const cont = document.querySelector(".layoutsection");
+	// 	const currentScroll = window.pageYOffset;
 
-		console.log("last scroll = "+lastScroll);
-		console.log("current scroll = "+currentScroll);
-		if (currentScroll > lastScroll) {
-			cont.classList.add("scroll-down");
-			cont.classList.remove("scroll-up");
-		} 
-		if (currentScroll < lastScroll) {
-			cont.classList.remove("scroll-down");
-			cont.classList.add("scroll-up");
-		}
-		if (currentScroll <= 0) {
-			cont.classList.remove("scroll-up");
-		}
-		lastScroll = currentScroll;
-	});
+	// 	console.log("last scroll = "+lastScroll);
+	// 	console.log("current scroll = "+currentScroll);
+	// 	if (currentScroll > lastScroll) {
+	// 		cont.classList.add("scroll-down");
+	// 		cont.classList.remove("scroll-up");
+	// 	} 
+	// 	if (currentScroll < lastScroll) {
+	// 		cont.classList.remove("scroll-down");
+	// 		cont.classList.add("scroll-up");
+	// 	}
+	// 	if (currentScroll <= 0) {
+	// 		cont.classList.remove("scroll-up");
+	// 	}
+	// 	lastScroll = currentScroll;
+	// });
 
 	return (
 		<div className='w-full transition duration-150 ease-linear font-fjalla bg-pritxt text-pribg dark:text-pritxt dark:bg-pribg'>
--- pages/skills.js
+++ pages/skills.js
@@ -16,7 +16,7 @@ const skills = () => {
 			<br />
 			<div className=' w-full grid grid-cols-2 md:grid-cols-1 sm:grid-cols-1 xsm:grid-cols-1 grid-flow-rows gap-4 place-items-center'>
 				<Certificate name={'Internet Fundamentals'} from={"Codedamn Community"} date={'12 Jan 2023'} link={'https://codedamn.com/user/k8pai'} imgLink={"/certificates/BasicsOfWeb.jpg"} />
-				{/* <Certificate name={'SpiritWolf'} desc={"This is an NFT project I worked on with Web3 works included."} date={'13 July 2022'} link={'http://spiritwolf.vercel.app'} imgLink="/spiritwolf.png" /> */}
+				<Certificate name={'HTML and CSS'} from={"Codedamn Community"} date={'17 Jan 2023'} link={'https://codedamn.com/user/k8pai'} imgLink={"/certificates/HtmlAndCss.jpg"} />
 				
 			</div>
 			
