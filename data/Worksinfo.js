9a754ef7cd8bbb6d41a6f25b361e292938766b7c
k8pai <thek8pai@gmail.com>
2023-01-28 19:41:10 +0530

    added data files

---
+++ data/Worksinfo.js
@@ -0,0 +1,18 @@
+const worksData=[
+	{
+		name: 'Todolist',
+		desc: 'This is a sample crud operations project to showcase my skills.',
+		date: '16 Dec 2022',
+		link: 'http://todolist-k8pai.vercel.app',
+		imgLink: '/todolist.png',
+	},
+	{
+		name: 'SpiritWolf',
+		desc: 'This is an NFT project I worked on with Web3 works included.',
+		date: '13 July 2022',
+		link: 'http://spiritwolf.vercel.app',
+		imgLink: '/spiritwolf.png',
+	},
+]
+
+export default worksData;
