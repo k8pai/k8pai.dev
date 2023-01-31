e69a383a78198cc95a29e4c405b21d6dab151bd9
k8pai <thek8pai@gmail.com>
2023-01-31 17:33:25 +0530

    mobile ui integration failed staus

--- components/Header.js
+++ components/Header.js
@@ -3,19 +3,21 @@ import { useTheme } from 'next-themes'
 import { IconContext } from 'react-icons'
 import React, { useEffect, useState } from 'react'
 import { BsSunFill, BsMoonStarsFill } from 'react-icons/bs'
-import { TbSignature } from 'react-icons/tb'
-import '../styles/header.module.css'
-import { SiAboutdotme, SiHomeadvisor } from 'react-icons/si'
-import { GiHamburgerMenu } from 'react-icons/gi'
-import { SlSocialSpotify } from 'react-icons/sl'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { SiHomeadvisor, SiAboutdotme } from 'react-icons/si'
 import { VscTools } from 'react-icons/vsc'
-import { CiFolderOn } from 'react-icons/ci'
+import { Logok8pai } from '../components/socialIcons/SocialIcons'
 
 
 export default function Header() {
 	
 	const {systemTheme, theme, setTheme} = useTheme();
+	const [showNav, setShowNav] = useState(false);
 	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		console.log(showNav);
+	}, [showNav])
 	
 	useEffect(() => {
 		setMounted(true);
@@ -27,81 +29,91 @@ export default function Header() {
 
 		if(currTheme == "dark"){
 			return(
-				<div>
-					<button className='link-hover transition duration-300 p-2 m-px rounded-lg tracking-wider z-50 menu-shadow' onClick={() => { setTheme('light') }}>
+				<div className='group relative flex justify-center items-center -space-x-3'>
+					{showNav? <button className='menu-shadow p-2 m-px rounded-lg tracking-wider z-50 flex items-center space-x-3' onClick={() => { setTheme('light') }}>
 						<IconContext.Provider value={{ color: "white", size:"1.6em", className: "global-class-name" }}>
 							<BsSunFill />
 						</IconContext.Provider>
-					</button>
-					<p className='sibling-element capitalize opacity-0 transition duration-200 select-none'>{theme}</p>
+						<span>Light</span>
+					</button>: null}
 				</div>
 			)
 		}else {
 			return (
-				<div>
-					<button className='link-hover transition duration-300 p-2 m-px rounded-lg tracking-wider z-50  menu-shadow' onClick={() => { setTheme('dark') }}>
+				<div className='group relative flex justify-center items-center -space-x-3'>
+					{showNav? <button className='menu-shadow p-2 m-px rounded-lg tracking-wider z-50 flex items-center space-x-3' onClick={() => { setTheme('dark') }}>
 						<IconContext.Provider value={{ color: "black", size:"1.6em", className: "global-class-name" }}>
 							<BsMoonStarsFill />
 						</IconContext.Provider>
-					</button>
-					<p className='sibling-element capitalize opacity-0 transition duration-200 select-none'>{theme}</p>
+						<span>Dark</span>
+					</button>: null}
 				</div>
-
 			)
 		}
 	}
 	return (
 		<div>
-			<div className='main-section font-semibold transition duration-300 mt-8'>
-				<ul className='h-full flex justify-center w-fit items-center space-x-12 rounded-full mx-auto style-none'>
-					<div className='relative flex flex-col items-center'>
-						<Link className='link-hover transition duration-300 p-2 m-px rounded-lg tracking-wider z-50 menu-shadow' href={''}>
+			<div className='main-section sticky top-0 left-0 w-full mx-auto bg-slate-300 dark:bg-pribg font-semibold transition duration-300 py-6'>
+				<ul className='h-full flex flex-col justify-between items-center rounded-full mx-4 space-y-6 style-none'>
+					<div className='w-full flex justify-between items-center'>
+						<div className='group relative flex items-center space-x-3 px-2'>
+							<Logok8pai />
+						</div>
+						<div className='flex-grow space-x-6 flex justify-center items-center w-full'>
+							<div className='group relative flex flex-col items-center'>
+								<Link className='link-hover transition duration-300 p-2 m-px rounded-lg tracking-wider z-50 menu-shadow' href={'/'}>
+									<IconContext.Provider value={{ size:"1.46em", className: "global-class-name text-pribg dark:text-pritxt" }}>
+										<SiHomeadvisor />
+									</IconContext.Provider>
+								</Link>
+								<p className='sibling-element capitalize opacity-0 transition duration-200 select-none'>Home</p>
+							</div>
+							<div className='group relative flex flex-col items-center'>
+								<Link className='link-hover transition duration-300 p-2 m-px rounded-lg tracking-wider z-50 menu-shadow' href={'/about'}>
+									<IconContext.Provider value={{ size:"1.6em", className: "global-class-name text-pribg dark:text-pritxt" }}>
+										<SiAboutdotme />
+									</IconContext.Provider>
+								</Link>
+								<p className='sibling-element capitalize opacity-0 transition duration-200 select-none'>about</p>
+							</div>
+							<div className='group relative flex flex-col items-center'>
+								<Link className='link-hover transition duration-300 p-2 m-px rounded-lg tracking-wider z-50 menu-shadow' href={'/skills'}>
+									<IconContext.Provider value={{ size:"1.6em", className: "global-class-name text-pribg dark:text-pritxt" }}>
+										<VscTools />
+									</IconContext.Provider>
+								</Link>
+								<p className='sibling-element capitalize opacity-0 transition duration-200 select-none'>skills</p>
+							</div>
+							{renderThemeSystem()}
+						</div>
+						<button onClick={() => setShowNav(value => !value)} className='transition duration-300 p-2 m-px rounded-lg tracking-wider z-50 menu-shadow' href={''}>
 							<IconContext.Provider value={{ size:"1.6em", className: "global-class-name text-pribg dark:text-pritxt" }}>
-								<GiHamburgerMenu />
+								{showNav? <AiOutlineClose />: <AiOutlineMenu />}
 							</IconContext.Provider>
-						</Link>
-						<p className='sibling-element capitalize opacity-0 transition duration-200 select-none'>menu</p>
+						</button>
 					</div>
-					<div className='group relative flex flex-col items-center'>
-						<Link className='link-hover transition duration-300 p-2 m-px rounded-lg tracking-wider z-50 menu-shadow' href={'/'}>
+					{showNav? 
+					<div className='w-full flex flex-col items-start space-y-6'>
+						<Link className='menu-shadow p-2 m-px rounded-lg tracking-wider z-50 flex items-center space-x-3' href={'/'}>
 							<IconContext.Provider value={{ size:"1.46em", className: "global-class-name text-pribg dark:text-pritxt" }}>
 								<SiHomeadvisor />
 							</IconContext.Provider>
+							<span>Home</span>
 						</Link>
-						<p className='sibling-element capitalize opacity-0 transition duration-200 select-none'>Home</p>
-					</div>
-					<div className='group relative flex flex-col items-center'>
-						<Link className='link-hover transition duration-300 p-2 m-px rounded-lg tracking-wider z-50 menu-shadow' href={'/about'}>
+						<Link className='menu-shadow p-2 m-px rounded-lg tracking-wider z-50 flex items-center space-x-3' href={'/about'}>
 							<IconContext.Provider value={{ size:"1.6em", className: "global-class-name text-pribg dark:text-pritxt" }}>
 								<SiAboutdotme />
 							</IconContext.Provider>
+							<span>About</span>
 						</Link>
-						<p className='sibling-element capitalize opacity-0 transition duration-200 select-none'>about</p>
-					</div>
-					<div className='group relative flex flex-col items-center'>
-						<Link className='link-hover transition duration-300 p-2 m-px rounded-lg tracking-wider z-50 menu-shadow' href={'/skills'}>
+						<Link className='menu-shadow p-2 m-px rounded-lg tracking-wider z-50 flex items-center space-x-3' href={'/skills'}>
 							<IconContext.Provider value={{ size:"1.6em", className: "global-class-name text-pribg dark:text-pritxt" }}>
 								<VscTools />
 							</IconContext.Provider>
+							<span>Skills</span>
 						</Link>
-						<p className='sibling-element capitalize opacity-0 transition duration-200 select-none'>skills</p>
-					</div>
-					{/* <Link className='p-3 rounded-full transition duration-300 hover:shadow-xl tracking-wider  hover:bg-pritxt hover:dark:bg-secbg' href={'/repos'}>
-						<IconContext.Provider value={{ size:"1.4em", className: "global-class-name text-pribg dark:text-pritxt" }}>
-							<CiFolderOn />
-						</IconContext.Provider>
-					</Link> */}
-					{/* <Link className='p-3 rounded-full transition duration-300 hover:shadow-xl tracking-wider  hover:bg-pritxt hover:dark:bg-secbg' href={'/spotify'}>
-						<IconContext.Provider value={{ size:"1.4em", className: "global-class-name text-pribg dark:text-pritxt" }}>
-							<SlSocialSpotify />
-						</IconContext.Provider>
-					</Link>
-					<Link className='p-3 rounded-full transition duration-300 hover:shadow-xl tracking-wider  hover:bg-pritxt hover:dark:bg-secbg' href={'/sign'}>
-						<IconContext.Provider value={{ size:"1.4em", className: "global-class-name text-pribg dark:text-pritxt" }}>
-							<TbSignature />
-						</IconContext.Provider>
-					</Link> */}
-					{renderThemeSystem()}
+						{renderThemeSystem()}
+					</div>: null}
 				</ul>
 			</div>
 
