e0cf81d1b7dbcc77797abc18c616da6ec9a45279
k8pai <thek8pai@gmail.com>
2023-05-08 23:54:51 +0530

    updated layout with info and tech skills

--- app/me/layout.tsx
+++ app/me/layout.tsx
@@ -1,53 +1,17 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { createContext, useState } from 'react';
 import { motion } from 'framer-motion';
-import { Metadata } from 'next';
+import { MdLocationPin } from 'react-icons/md';
+import TechSkills from '../../components/TechSkills';
 
-export const metadata: Metadata = {
-	title: {
-		default: 'Sudarsan k pai',
-		template: '%s | Lee Robinson',
-	},
-	description: 'Developer, writer, and creator.',
-	openGraph: {
-		title: 'Lee Robinson',
-		description: 'Developer, writer, and creator.',
-		url: 'https://leerob.io',
-		siteName: 'Lee Robinson',
-		images: [
-			{
-				url: 'https://leerob.io/og.jpg',
-				width: 1920,
-				height: 1080,
-			},
-		],
-		locale: 'en-US',
-		type: 'website',
-	},
-	robots: {
-		index: true,
-		follow: true,
-		googleBot: {
-			index: true,
-			follow: true,
-			'max-video-preview': -1,
-			'max-image-preview': 'large',
-			'max-snippet': -1,
-		},
-	},
-	twitter: {
-		title: 'Lee Robinson',
-		card: 'summary_large_image',
-	},
-	icons: {
-		shortcut: '/favicon.ico',
-	},
-	verification: {
-		google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
-		yandex: '14d2e73487fa6c71',
-	},
-};
+interface meContextType {
+	selected: String;
+}
+
+export const meContext = createContext<meContextType>({
+	selected: '',
+});
 
 export default function RootLayout({
 	children,
@@ -61,37 +25,83 @@ export default function RootLayout({
 		resume: { name: 'resume' },
 	};
 
-	useEffect(() => {
-		Object.entries(options).map(([el, { name }]) => {
-			console.log(el, name);
-		});
-	}, []);
+	const skills = [
+		'HTML',
+		'CSS',
+		'Javascript',
+		'Typescript',
+		'Java',
+		'C++',
+		'Objective C',
+		'React',
+		'Redux',
+		'Next JS',
+		'Framer Motion',
+		'Tailwindcss',
+		'Mongo',
+		'Node js',
+		'Express js',
+		'Redis',
+	];
+
 	return (
-		<div>
-			<div className="flex space-x-3 border border-slate-400 p-2 rounded-lg">
-				{Object.entries(options).map(([el, { name }], elXid) => {
-					return (
-						<button
-							key={elXid}
-							className={`relative py-2 shadow-md rounded-md ${
-								name === selected ? 'selected' : ''
-							}`}
-							onClick={() => setSelected(name)}
-						>
-							<span className={`relative px-3 py-2 `}>
-								{name}
-								{name === selected ? (
-									<motion.div
-										className="absolute inset-0 bg-[#4338CA] rounded-md z-[-1]"
-										layoutId="underline"
-									/>
-								) : null}
-							</span>
-						</button>
-					);
-				})}
+		<div className="space-y-10">
+			<div className="border border-slate-400 py-4 px-5 rounded-lg">
+				<div className="flex space-x-3 items-center">
+					<h1 className="font-serif font-extrabold text-3xl capitalize ">
+						Sudarsan k pai
+					</h1>
+					<div className="px-2 py-1 text-sm bg-cyan-300 bg-opacity-70 rounded-md">
+						<span>student </span>
+					</div>
+					<div className="px-2 py-1 text-sm bg-cyan-300 bg-opacity-70 rounded-md">
+						<span>looking for a job</span>
+					</div>
+				</div>
+				<div className="status mt-4">
+					<div className="flex text-slate-200">
+						<span className="text-lg">Student | Web Developer</span>
+					</div>
+					<div className="flex items-center space-x-1 text-slate-400">
+						<MdLocationPin />
+						<span className="text-lg">Banglore, India</span>
+					</div>
+				</div>
+				<div className="flex flex-wrap mt-5">
+					{skills.map((el, elXid) => {
+						return <TechSkills key={elXid}>{el}</TechSkills>;
+					})}
+				</div>
 			</div>
-			{children}
+
+			<meContext.Provider value={{ selected: selected }}>
+				<div className="flex space-x-7 border border-slate-400 py-3 px-5 rounded-lg">
+					{Object.entries(options).map(([el, { name }], elXid) => {
+						return (
+							<button
+								key={elXid}
+								className={`relative py-2 shadow-md rounded-md ${
+									name === selected ? 'selected' : ''
+								}`}
+								onClick={() => setSelected(name)}
+							>
+								<span
+									className={`relative px-3 py-2 tracking-wider font-semibold`}
+								>
+									{name}
+									{name === selected ? (
+										<motion.div
+											className="absolute inset-0 bg-[#4338CA] rounded-md z-[-1]"
+											layoutId="underline"
+										/>
+									) : null}
+								</span>
+							</button>
+						);
+					})}
+				</div>
+				{children}
+			</meContext.Provider>
 		</div>
 	);
 }
