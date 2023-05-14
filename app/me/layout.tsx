714a2db3dbd0a48db4dd2b388920eea34ed9e541
k8pai <thek8pai@gmail.com>
2023-05-14 23:08:23 +0530

    updated with layout structure and removed skill tags section

--- app/me/layout.tsx
+++ app/me/layout.tsx
@@ -4,6 +4,8 @@ import React, { createContext, useState } from 'react';
 import { motion } from 'framer-motion';
 import { MdLocationPin } from 'react-icons/md';
 import TechSkills from '../../components/TechSkills';
+import Image from 'next/image';
+import profilePhoto from '../../public/developer.png';
 
 interface meContextType {
 	selected: String;
@@ -18,83 +20,64 @@ export default function RootLayout({
 }: {
 	children: React.ReactNode;
 }) {
-	const [selected, setSelected] = useState<String>('about');
+	const [selected, setSelected] = useState<String>('portfolio');
 
 	const options = {
-		about: { name: 'about' },
+		portfolio: { name: 'portfolio' },
 		resume: { name: 'resume' },
 	};
 
-	const skills = [
-		'HTML',
-		'CSS',
-		'Javascript',
-		'Typescript',
-		'Java',
-		'C++',
-		'Objective C',
-		'React',
-		'Redux',
-		'Next JS',
-		'Framer Motion',
-		'Tailwindcss',
-		'Mongo',
-		'Node js',
-		'Express js',
-		'Redis',
-	];
-
 	return (
-		<div className="space-y-10">
-			<div className="border border-slate-400 py-4 px-5 rounded-lg">
-				<div className="flex space-x-3 items-center">
-					<h1 className="font-serif font-extrabold text-3xl capitalize ">
-						Sudarsan k pai
-					</h1>
-					<div className="px-2 py-1 text-sm bg-cyan-300 bg-opacity-70 rounded-md">
-						<span>student </span>
-					</div>
-					<div className="px-2 py-1 text-sm bg-cyan-300 bg-opacity-70 rounded-md">
-						<span>looking for a job</span>
-					</div>
+		<div className="space-y-5">
+			<div className="py-2 px-3 rounded-lg flex items-start">
+				<div className="relative max-w-[150px] max-h-[150px] p-3 hidden sm:block">
+					<Image
+						src={profilePhoto}
+						className="aspect-square rounded-full"
+						alt="profile picture"
+					/>
 				</div>
-				<div className="status mt-4">
-					<div className="flex text-slate-200">
-						<span className="text-lg">Student | Web Developer</span>
+				<div className="p-3">
+					<div className="flex space-x-3 items-center">
+						<h1 className="font-serif font-extrabold text-3xl capitalize ">
+							Sudarsan k pai
+						</h1>
 					</div>
-					<div className="flex items-center space-x-1 text-slate-400">
-						<MdLocationPin />
-						<span className="text-lg">Banglore, India</span>
+					<div className="status mt-4">
+						<div className="flex text-slate-200">
+							<span className="text-lg">
+								Student | Web Developer
+							</span>
+						</div>
+						<div className="flex items-center space-x-1 text-slate-400">
+							<MdLocationPin />
+							<span className="text-lg">Banglore, India</span>
+						</div>
 					</div>
-				</div>
-				<div className="flex flex-wrap mt-5">
-					{skills.map((el, elXid) => {
-						return <TechSkills key={elXid}>{el}</TechSkills>;
-					})}
 				</div>
 			</div>
-
 			<meContext.Provider value={{ selected: selected }}>
-				<div className="flex space-x-7 border border-slate-400 py-3 px-5 rounded-lg">
+				<div className="flex space-x-7 p-2 rounded-md">
 					{Object.entries(options).map(([el, { name }], elXid) => {
 						return (
 							<button
 								key={elXid}
-								className={`relative py-2 shadow-md rounded-md ${
+								className={`relative py-2 shadow-md ${
 									name === selected ? 'selected' : ''
 								}`}
 								onClick={() => setSelected(name)}
 							>
 								<span
-									className={`relative px-3 py-2 tracking-wider font-semibold`}
+									className={`relative px-3 py-2 tracking-wider capitalize font-semibold`}
 								>
-									{name}
+									<motion.div className="absolute inset-0 bg-[#181818] rounded-md z-[-5]" />
 									{name === selected ? (
 										<motion.div
-											className="absolute inset-0 bg-[#4338CA] rounded-md z-[-1]"
+											className="absolute inset-0 bg-[#4338CA] bg-opacity-50 rounded-md z-[-1]"
 											layoutId="underline"
 										/>
 									) : null}
+									{name}
 								</span>
 							</button>
 						);
