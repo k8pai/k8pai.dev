6f3ee9f85c940065e1f5adacfca8eb5763a92c47
k8pai <thek8pai@gmail.com>
2023-05-14 23:11:53 +0530

    updated to include components along with color of the same in skills tag

--- components/TechSkills.tsx
+++ components/TechSkills.tsx
@@ -1,21 +1,29 @@
 import { Icon } from 'next/dist/lib/metadata/types/metadata-types';
 import React from 'react';
-import { IconType } from 'react-icons/lib';
+import { IconContext, IconType } from 'react-icons/lib';
 
 export default function TechSkills({
 	Component,
+	color,
 	children,
 }: {
 	Component?: IconType;
+	color?: string;
 	children: React.ReactNode;
 }) {
 	return (
 		<div
 			className={`${
-				Component ? 'flex items-center ' : 'font-semibold font-mono'
+				Component
+					? 'flex items-center space-x-3'
+					: 'font-semibold font-mono'
 			} px-3 py-1 w-fit m-1 border border-slate-500/40 rounded-lg bg-[#181818] text-slate-200`}
 		>
-			{Component ? <Component /> : null}
+			{Component ? (
+				<IconContext.Provider value={{ color: color }}>
+					<Component />
+				</IconContext.Provider>
+			) : null}
 			<span>{children}</span>
 		</div>
 	);
