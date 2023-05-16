aca3fc85be15724458f7767087fc127997245187
k8pai <thek8pai@gmail.com>
2023-05-16 23:56:22 +0530

    updated with social links

--- app/me/layout.tsx
+++ app/me/layout.tsx
@@ -1,14 +1,30 @@
 'use client';
 
-import React, { createContext, useState } from 'react';
+import React, { Component, createContext, useState } from 'react';
 import { motion } from 'framer-motion';
-import { MdLocationPin } from 'react-icons/md';
+import { MdAdd, MdLocationPin } from 'react-icons/md';
 import TechSkills from '../../components/TechSkills';
 import Image from 'next/image';
 import profilePhoto from '../../public/developer.png';
+import { IconContext, IconType } from 'react-icons';
+import { ImDownload } from 'react-icons/im';
+import {
+	SiDiscord,
+	SiGithub,
+	SiGmail,
+	SiInstagram,
+	SiLinkedin,
+} from 'react-icons/si';
+import Link from 'next/link';
 
 interface meContextType {
 	selected: String;
+}
+
+interface socialLinksType {
+	color: string;
+	Component: IconType;
+	href: string;
 }
 
 export const meContext = createContext<meContextType>({
@@ -21,11 +37,40 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	const [selected, setSelected] = useState<String>('portfolio');
+	const [showSocials, setShowSocials] = useState<boolean>(false);
 
 	const options = {
 		portfolio: { name: 'portfolio' },
 		resume: { name: 'resume' },
 	};
+
+	const socialLinks: socialLinksType[] = [
+		{
+			Component: SiInstagram,
+			color: '#E4405F',
+			href: 'https://instagram.com/_k8pai',
+		},
+		{
+			Component: SiLinkedin,
+			color: '#0A66C2',
+			href: 'https://linkedin.com/in/k8pai',
+		},
+		{
+			Component: SiGithub,
+			color: '#fff',
+			href: 'https://github.com/k8pai',
+		},
+		{
+			Component: SiGmail,
+			color: '#EA4335',
+			href: 'mailto:thek8pai@gmail.com',
+		},
+		{
+			Component: SiDiscord,
+			color: '#5865F2',
+			href: 'https://discord.com/users/898949804024012850',
+		},
+	];
 
 	return (
 		<div className="space-y-5">
@@ -43,7 +88,7 @@ export default function RootLayout({
 							Sudarsan k pai
 						</h1>
 					</div>
-					<div className="status mt-4">
+					<div className="status mt-2">
 						<div className="flex text-slate-200">
 							<span className="text-lg">
 								Student | Web Developer
@@ -53,6 +98,27 @@ export default function RootLayout({
 							<MdLocationPin />
 							<span className="text-lg">Banglore, India</span>
 						</div>
+					</div>
+					<div className="flex mt-3 items-center space-x-3">
+						{socialLinks.map((el, elXid) => {
+							const { color, Component, href } = el;
+							return (
+								<Link
+									key={elXid}
+									href={href}
+									className="p-2 transition-all duration-200 bg-[#181818] hover:scale-110 hover:shadow-md rounded-md"
+								>
+									<IconContext.Provider
+										value={{
+											size: '1.5em',
+											color: color,
+										}}
+									>
+										<Component />
+									</IconContext.Provider>
+								</Link>
+							);
+						})}
 					</div>
 				</div>
 			</div>
@@ -82,6 +148,22 @@ export default function RootLayout({
 							</button>
 						);
 					})}
+					<span className={'flex-grow'}></span>
+					<button
+						className={`group relative hover:shadow-md ${
+							selected === 'resume' ? 'block' : 'hidden'
+						}`}
+					>
+						<IconContext.Provider
+							value={{
+								size: '1.5em',
+								className:
+									'relative tracking-wider capitalize bg-[#181818] rounded-md font-semibold m-2',
+							}}
+						>
+							<ImDownload />
+						</IconContext.Provider>
+					</button>
 				</div>
 				{children}
 			</meContext.Provider>
