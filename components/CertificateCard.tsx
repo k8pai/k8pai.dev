75017fcd2a6a08126fb9d82ac8c9117b58706e98
k8pai <thek8pai@gmail.com>
2023-05-17 22:54:41 +0530

    updated with grid and details layout in portfolio certicate section with responsive stylingss

--- components/CertificateCard.tsx
+++ components/CertificateCard.tsx
@@ -2,26 +2,34 @@ import Link from 'next/link';
 import React from 'react';
 import { IconContext, IconType } from 'react-icons/lib';
 
-interface certificateDataType {
+export interface certificateDataType {
 	name: string;
 	issue: string;
 	href: string;
 }
 
-interface certificateCoponentType {
+export interface certificateCoponentType {
 	Component: IconType;
 	color: string;
 }
 
 export default function CertificateCard({
+	isGrid,
 	data: { name, issue, href },
 	Component: { Component, color },
 }: {
+	isGrid: boolean;
 	data: certificateDataType;
 	Component: certificateCoponentType;
 }) {
 	return (
-		<div className="rounded-md  bg-[#181818] p-3 w-full">
+		<div
+			className={`${
+				!isGrid
+					? 'flex flex-col md:flex-row md:items-start space-x-3'
+					: null
+			} rounded-md bg-[#181818] p-3 w-full`}
+		>
 			<div className="imageSection p-3">
 				<IconContext.Provider
 					value={{
@@ -34,10 +42,14 @@ export default function CertificateCard({
 				</IconContext.Provider>
 			</div>
 			<div className="ml-2">
-				<h1 className="font-semibold tracking-wider text-2xl py-2">
+				<h1
+					className={`font-semibold tracking-wider text-2xl ${
+						!isGrid ? 'py-1' : 'py-2'
+					}`}
+				>
 					{name}
 				</h1>
-				<h2 className="py-1">{issue}</h2>
+				<h2 className={`py-0`}>{issue}</h2>
 				<Link
 					href={href}
 					className="font-semibold text-gray-400 mt-3 flex items-start md:items-center md:space-x-1"
--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import { IconContext } from 'react-icons/lib';
 import { IoLogoJavascript } from 'react-icons/io';
 import { SiGithub, SiNextdotjs, SiReact, SiTailwindcss } from 'react-icons/si';
 import Link from 'next/link';
 import ProjectCard from './ProjectCard';
 import CertificateCard from './CertificateCard';
+import { CiGrid41, CiGrid2H } from 'react-icons/ci';
+import { certificatesInfo, certificatesInfoType } from '../lib/info';
 
 export default function Portfolio() {
+	const [isGrid, setIsGrid] = useState<boolean>(true);
+	const [showFullCertificates, setShowFullCertficates] =
+		useState<boolean>(false);
+	const [certificate, setCertificate] = useState<certificatesInfoType[]>(
+		certificatesInfo.slice(0, 4),
+	);
+
+	const handleShowCertificates = () => {
+		if (certificate.length === 4) {
+			setShowFullCertficates(true);
+			setCertificate(certificatesInfo);
+		} else {
+			setShowFullCertficates(false);
+			setCertificate(certificatesInfo.slice(0, 4));
+		}
+	};
+
 	return (
 		<div className="">
 			{/* Projects section */}
@@ -33,46 +54,52 @@ export default function Portfolio() {
 					</Link>
 				</div>
 				<div className="grid gap-4 grid-cols-1 lg:grid-cols-2 place-items-center p-3">
-					<ProjectCard
-						name={'Portfolio'}
-						Components={[
-							{ Component: SiTailwindcss, color: '#06B6D4' },
-							{ Component: SiNextdotjs, color: '#ffffff' },
-						]}
-					/>
-					<ProjectCard
-						name={'Portfolio'}
-						Components={[
-							{ Component: SiTailwindcss, color: '#06B6D4' },
-							{ Component: SiNextdotjs, color: '#ffffff' },
-						]}
-					/>
-					<ProjectCard
-						name={'Portfolio'}
-						Components={[
-							{ Component: SiTailwindcss, color: '#06B6D4' },
-							{ Component: SiNextdotjs, color: '#ffffff' },
-						]}
-					/>
-					<ProjectCard
-						name={'Portfolio'}
-						Components={[
-							{ Component: SiTailwindcss, color: '#06B6D4' },
-							{ Component: SiNextdotjs, color: '#ffffff' },
-						]}
-					/>
+					{Array.from({ length: 4 }, (_, id) => {
+						return (
+							<ProjectCard
+								key={id}
+								name={'Portfolio'}
+								Components={[
+									{
+										Component: SiTailwindcss,
+										color: '#06B6D4',
+									},
+									{
+										Component: SiNextdotjs,
+										color: '#ffffff',
+									},
+								]}
+							/>
+						);
+					})}
 				</div>
 			</div>
 
 			{/* Certificates section */}
 			<div className="education mt-10">
-				<div className="flex items-center justify-between p-3">
+				<div className="flex items-center justify-between p-3 space-x-3">
 					<h1 className=" font-bold tracking-wide text-2xl capitalize">
 						Certificates
 					</h1>
-					<Link
-						href={'https://github.com/k8pai'}
+					<span className={'flex-grow'}></span>
+					<button
+						className={`group relative bg-[#202020] rounded-md hover:shadow-md hidden lg:block`}
+						onClick={() => setIsGrid(!isGrid)}
+					>
+						<IconContext.Provider
+							value={{
+								size: '1.5em',
+								className:
+									'relative tracking-wider capitalize font-semibold m-2',
+							}}
+						>
+							{isGrid ? <CiGrid2H /> : <CiGrid41 />}
+						</IconContext.Provider>
+					</button>
+					<button
+						type="button"
 						className="transition-all md:text-blue-400 md:hover:text-cyan-400 bg-[#181818] p-2 md:bg-transparent md:p-0 rounded-md"
+						onClick={handleShowCertificates}
 					>
 						<IconContext.Provider
 							value={{
@@ -83,44 +110,28 @@ export default function Portfolio() {
 							<SiGithub />
 						</IconContext.Provider>
 						<span className="hidden md:block md:font-semibold">
-							Show More
+							{showFullCertificates ? 'Show Less' : 'Show More'}
 						</span>
-					</Link>
+					</button>
 				</div>
-				<div className="grid gap-4 grid-cols-1 lg:grid-cols-2 place-items-center p-3">
-					<CertificateCard
-						data={{
-							name: 'javascript',
-							issue: 'issued on Dec 16th, 2022',
-							href: 'javascript.info',
-						}}
-						Component={{
-							Component: IoLogoJavascript,
-							color: '#F7DF1E',
-						}}
-					/>
-					<CertificateCard
-						data={{
-							name: 'Advance Javascript',
-							issue: 'issued on Dec 16th, 2022',
-							href: 'javascript.info',
-						}}
-						Component={{
-							Component: IoLogoJavascript,
-							color: '#F7DF1E',
-						}}
-					/>
-					<CertificateCard
-						data={{
-							name: 'React',
-							issue: 'issued on Dec 16th, 2022',
-							href: 'javascript.info',
-						}}
-						Component={{
-							Component: SiReact,
-							color: '#61DAFB',
-						}}
-					/>
+				<div
+					className={`${
+						isGrid
+							? 'grid gap-4 grid-cols-1 lg:grid-cols-2  place-items-center'
+							: 'w-full flex flex-col space-y-4'
+					} p-3`}
+				>
+					{certificate.map((el, elXid) => {
+						const { data, Component } = el;
+						return (
+							<CertificateCard
+								key={elXid}
+								isGrid={isGrid}
+								data={data}
+								Component={Component}
+							/>
+						);
+					})}
 				</div>
 			</div>
 		</div>
