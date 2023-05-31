5669e8a7768c403010955eed33bd20d3afa43d4f
k8pai <thek8pai@gmail.com>
2023-06-01 02:45:50 +0530

    updated with prisma connection and guestbook queries

---
+++ lib/prisma/guestbook.ts
@@ -0,0 +1,31 @@
+import prisma from '.';
+
+export async function getComments() {
+	try {
+		const data = await prisma.guestbook.findMany({
+			orderBy: {
+				updated_at: 'desc',
+			},
+		});
+		return { data };
+	} catch (error) {
+		return { error };
+	}
+}
+
+interface userType {
+	id: string;
+	email: string;
+	body: string;
+	created_by: string;
+	updated_at?: any;
+}
+
+export async function createComments(user: userType) {
+	try {
+		const data = await prisma.guestbook.create({ data: user });
+		return { data };
+	} catch (error) {
+		return { error };
+	}
+}
