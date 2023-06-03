28d00933d1702a4ace01c046b5fce22cdc2877bf
k8pai <thek8pai@gmail.com>
2023-06-03 23:53:31 +0530

    modified ui

--- components/CertificateCard.tsx
+++ components/CertificateCard.tsx
@@ -1,26 +1,18 @@
 import Link from 'next/link';
 import React from 'react';
 import { IconContext, IconType } from 'react-icons/lib';
-
-export interface certificateDataType {
-	name: string;
-	issue: string;
-	href: string;
-}
-
-export interface certificateCoponentType {
-	Component: IconType;
-	color: string;
-}
+import { BsDot } from 'react-icons/bs';
+import { certificatesInfoType } from '../lib/info';
 
 export default function CertificateCard({
 	isGrid,
-	data: { name, issue, href },
-	Component: { Component, color },
+	certificate: {
+		data: { name, issuer, issueDate, issuerUrl, href },
+		Component: { Component, color },
+	},
 }: {
 	isGrid: boolean;
-	data: certificateDataType;
-	Component: certificateCoponentType;
+	certificate: certificatesInfoType;
 }) {
 	return (
 		<div
@@ -49,7 +41,23 @@ export default function CertificateCard({
 				>
 					{name}
 				</h1>
-				<h2 className={`py-0`}>{issue}</h2>
+				<div className={`py-0 flex items-center`}>
+					<span>{issueDate}</span>
+					<IconContext.Provider
+						value={{
+							size: '1.5em',
+							className: 'global-class-name',
+						}}
+					>
+						<BsDot />
+					</IconContext.Provider>
+					<Link
+						className="text-cyan-500 hover:text-cyan-400 transition-all"
+						href={issuerUrl}
+					>
+						{issuer}
+					</Link>
+				</div>
 				<Link
 					href={href}
 					className="font-semibold text-gray-400 mt-3 flex items-start md:items-center md:space-x-1"
