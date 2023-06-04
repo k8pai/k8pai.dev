2855372032efe1a943b22ede90b4544accbd1cba
k8pai <thek8pai@gmail.com>
2023-06-05 01:24:23 +0530

    updated with styles

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Comments = ({ comment: { created_by, body } }: { comment: any }) => {
 	return (
-		<div className="px-3 py-2">
+		<div className="px-1 py-2">
 			<div className="font-semibold py-1 text-neutral-500">
 				{created_by}
 			</div>
