96a6cdc01cb3ca85f000684132cfb3cc4a47e0b6
k8pai <thek8pai@gmail.com>
2023-06-07 23:50:16 +0530

    updated visit link

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -2,30 +2,44 @@ import React from 'react';
 import { IconContext } from 'react-icons/lib';
 import { projectsInfoType } from '../lib/info';
 import Image from 'next/image';
+import Link from 'next/link';
+import { CiLink } from 'react-icons/ci';
 
 export default function ProjectCard({
-	project: { name, imageUrl, Components },
+	project: { name, imageUrl, projectUrl, Components },
 }: {
 	project: projectsInfoType;
 }) {
 	return (
-		<div className="rounded-md  bg-[#181818] p-3 w-full">
+		<div className="rounded-md group bg-[#181818] p-3 w-full">
 			<div className="imageSection relative aspect-video bg-[#111010] rounded-md">
 				<Image src={imageUrl} alt={`${name} preview`} />
 			</div>
 			<div className="content-section">
-				<div className="projectname md:mt-3 font-semibold tracking-wider text-base md:text-2xl py-1 md:py-2">
-					{name}
+				<div className="projectname flex justify-between items-center md:mt-3 font-semibold tracking-wider text-base md:text-2xl py-1 md:py-2">
+					<span>{name}</span>
+					{projectUrl && (
+						<Link
+							href={projectUrl}
+							className="space-x-2 flex transition duration-200 ease-in opacity-0 group-hover:opacity-100 font-mono text-xs"
+						>
+							<span>visit</span>
+							<IconContext.Provider value={{ size: '1.5em' }}>
+								<CiLink />
+							</IconContext.Provider>
+						</Link>
+					)}
 				</div>
-				<div className="projectdetails p-1 md:mt-2 flex items-start md:items-center md:space-x-1">
+				<div className="projectdetails p-1 md:mt-2 flex items-start md:items-center space-x-3">
 					{Components?.map((el, elXid) => {
 						const { Component, color } = el;
 						return (
 							<IconContext.Provider
 								key={elXid}
 								value={{
+									size: '1.3em',
 									color: color,
-									className: 'react-icons mr-2',
+									className: 'react-icons',
 								}}
 							>
 								<Component />
