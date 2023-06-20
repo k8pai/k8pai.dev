beb79c4406339eb1a9e787c031bac049eae52204
k8pai <thek8pai@gmail.com>
2023-06-20 23:57:54 +0530

    added contentlayer for mdx files and blogs

---
+++ contentlayer.config.ts
@@ -0,0 +1,66 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+import remarkGfm from 'remark-gfm';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
+export const SolutionPosts = defineDocumentType(() => ({
+	name: 'Solutions',
+	filePathPattern: `**/*.mdx`,
+	contentType: 'mdx',
+	fields: {
+		title: {
+			type: 'string',
+			required: true,
+		},
+		date: {
+			type: 'date',
+			required: true,
+		},
+	},
+	computedFields: {
+		url: {
+			type: 'string',
+			resolve: (post) => `${post._raw.flattenedPath}`,
+		},
+	},
+}));
+
+export default makeSource({
+	contentDirPath: 'contents',
+	documentTypes: [SolutionPosts],
+	mdx: {
+		remarkPlugins: [remarkGfm],
+		rehypePlugins: [
+			rehypeSlug,
+			[
+				rehypePrettyCode,
+				{
+					theme: 'one-dark-pro',
+					onVisitLine(node) {
+						// Prevent lines from collapsing in `display: grid` mode, and allow empty
+						// lines to be copy/pasted
+						if (node.children.length === 0) {
+							node.children = [{ type: 'text', value: ' ' }];
+						}
+					},
+					onVisitHighlightedLine(node) {
+						node.properties.className.push('line--highlighted');
+					},
+					onVisitHighlightedWord(node) {
+						node.properties.className = ['word--highlighted'];
+					},
+				},
+			],
+			[
+				rehypeAutolinkHeadings,
+				{
+					properties: {
+						className: ['anchor'],
+					},
+				},
+			],
+		],
+	},
+});
