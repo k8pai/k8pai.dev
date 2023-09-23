7608f879e478c29d0771dad7fb14d7f23551506b
k8pai <thek8pai@gmail.com>
2023-09-23 12:31:13 +0530

    Implemented redirects for simplified and generalized social links

--- next.config.js
+++ next.config.js
@@ -7,6 +7,30 @@ const nextConfig = {
 	images: {
 		domains: ['github.com', 'avatars.githubusercontent.com'],
 	},
+	async redirects() {
+		return [
+			{
+				source: '/discord',
+				destination: 'https://discord.com/users/898949804024012850',
+				permanent: true,
+			},
+			{
+				source: '/linkedin',
+				destination: 'https://linkedin.com/in/k8pai',
+				permanent: true,
+			},
+			{
+				source: '/github',
+				destination: 'https://github.com/k8pai',
+				permanent: true,
+			},
+			{
+				source: '/wa',
+				destination: 'https://wa.me/7994648190',
+				permanent: true,
+			},
+		];
+	},
 };
 
 module.exports = withContentlayer(nextConfig);
