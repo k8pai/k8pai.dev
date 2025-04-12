0d23477b74b3aff9494ea19a37fa560fef29e825
k8pai <thek8pai@gmail.com>
2025-04-12 23:57:41 +0530

    fix: updated new project calendar into the portfolio

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import Link from 'next/link'
-import Logo from './Logo'
 import { SiDiscord, SiGithub, SiLinkedin } from 'react-icons/si'
+import Logo from './Logo'
 
 const Footer = () => {
     return (
--- data/projects.ts
+++ data/projects.ts
@@ -1,179 +1,214 @@
-import todolist from 'public/todolist_thumb.png';
-import pinit from 'public/pinit_thumb.png';
-import tailwindInputs from 'public/tailwind_inputs_thumb.png';
-import linkwrap from 'public/linkwrap_thumb.png';
+import calendar from 'public/calendar_thumb.png'
+import linkwrap from 'public/linkwrap_thumb.png'
+import pinit from 'public/pinit_thumb.png'
+import tailwindInputs from 'public/tailwind_inputs_thumb.png'
+import todolist from 'public/todolist_thumb.png'
 import {
-	SiNextdotjs,
-	SiTypescript,
-	SiTailwindcss,
-	SiMongodb,
-	SiPrisma,
-	SiFramer,
-	SiHtml5,
-	SiJavascript,
-	SiAuth0,
-	SiNodedotjs,
-	SiGmail,
-	SiGoogle,
-} from 'react-icons/si';
-import { projectsType } from 'typings';
+    SiAuth0,
+    SiDatefns,
+    SiFramer,
+    SiGmail,
+    SiGoogle,
+    SiHtml5,
+    SiJavascript,
+    SiMongodb,
+    SiNextdotjs,
+    SiNodedotjs,
+    SiPrisma,
+    SiShadcnui,
+    SiTailwindcss,
+    SiTypescript,
+} from 'react-icons/si'
+import { projectsType } from 'typings'
 
 export const projects: projectsType[] = [
-	{
-		name: 'Gmail-Automation',
-		imageUrl: pinit,
-		projectUrl: '',
-		githubUrl: 'https://github.com/k8pai/gmail-automation',
-		Components: [
-			{
-				Component: SiJavascript,
-				color: '#3178C6',
-			},
-			{
-				Component: SiNodedotjs,
-				color: '#339933',
-			},
-			{
-				Component: SiGmail,
-				color: '#EA4335',
-			},
-			{
-				Component: SiGoogle,
-				color: '#4285F4',
-			},
-			{
-				Component: SiAuth0,
-				color: '#EB5424',
-			},
-		],
-	},
-	{
-		name: 'Nextauth-cli',
-		imageUrl: pinit,
-		projectUrl: 'https://www.npmjs.com/package/nextauth-cli',
-		githubUrl: 'https://github.com/k8pai/nextauth-cli',
-		Components: [
-			{
-				Component: SiTypescript,
-				color: '#3178C6',
-			},
-			{
-				Component: SiAuth0,
-				color: '#EB5424',
-			},
-		],
-	},
-	{
-		name: 'Linkwrap',
-		imageUrl: linkwrap,
-		projectUrl: 'https://linkwrap.k8pai.dev',
-		githubUrl: 'https://github.com/k8pai/linkwrap',
-		Components: [
-			{
-				Component: SiNextdotjs,
-				color: '#ffffff',
-				darkColor: '#000000',
-			},
-			{
-				Component: SiTailwindcss,
-				color: '#06B6D4',
-			},
-			{
-				Component: SiMongodb,
-				color: '#47A248',
-			},
-			{
-				Component: SiPrisma,
-				color: '#2D3748',
-			},
-			{
-				Component: SiFramer,
-				color: '#0055FF',
-			},
-		],
-	},
-	{
-		name: 'Tailwind Inputs',
-		imageUrl: tailwindInputs,
-		projectUrl: 'https://ti-docs-2.vercel.app',
-		Components: [
-			{
-				Component: SiNextdotjs,
-				color: '#ffffff',
-			},
-			{
-				Component: SiTailwindcss,
-				color: '#06B6D4',
-			},
-			{
-				Component: SiFramer,
-				color: '#0055FF',
-			},
-		],
-	},
-	{
-		name: 'Todolist',
-		imageUrl: todolist,
-		projectUrl: 'https://todolist-k8pai.vercel.app',
-		githubUrl: 'https://github.com/k8pai/todolist',
-		Components: [
-			{
-				Component: SiNextdotjs,
-				color: '#ffffff',
-			},
-			{
-				Component: SiTailwindcss,
-				color: '#06B6D4',
-			},
-		],
-	},
-	{
-		name: 'Pinit (Chrome Extention)',
-		imageUrl: pinit,
-		// projectUrl: 'https://todolist-k8pai.vercel.app',
-		githubUrl: 'https://github.com/k8pai/pinit',
-		Components: [
-			{
-				Component: SiHtml5,
-				color: '#ffffff',
-			},
-			{
-				Component: SiTailwindcss,
-				color: '#06B6D4',
-			},
-			{
-				Component: SiJavascript,
-				color: '#06B6D4',
-			},
-		],
-	},
-	// {
-	// 	name: 'Portfolio',
-	// 	imageUrl: portfolio,
-	// 	projectUrl: 'https://k8pai.dev',
-	// 	githubUrl: 'https://github.com/k8pai/k8pai.dev',
-	// 	Components: [
-	// 		{
-	// 			Component: SiNextdotjs,
-	// 			color: '#ffffff',
-	// 			darkColor: '#000000',
-	// 		},
-	// 		{
-	// 			Component: SiTailwindcss,
-	// 			color: '#06B6D4',
-	// 		},
-	// 		{
-	// 			Component: SiMongodb,
-	// 			color: '#47A248',
-	// 		},
-	// 		{
-	// 			Component: SiPrisma,
-	// 			color: '#2D3748',
-	// 		},
-	// 		{
-	// 			Component: SiFramer,
-	// 			color: '#0055FF',
-	// 		},
-	// 	],
-	// },
-];
+    {
+        name: 'calendar',
+        imageUrl: calendar,
+        projectUrl: 'https://calendar.k8pai.dev',
+        githubUrl: 'https://github.com/k8pai/calendar',
+        Components: [
+            {
+                Component: SiTypescript,
+                color: '#3178C6',
+            },
+            {
+                Component: SiShadcnui,
+                color: '#000000',
+            },
+            {
+                Component: SiDatefns,
+                color: '#770C56',
+            },
+            {
+                Component: SiTailwindcss,
+                color: '#06B6D4',
+            },
+            {
+                Component: SiFramer,
+                color: '#0055FF',
+            },
+            {
+                Component: SiNextdotjs,
+                color: '#000000',
+            },
+        ],
+    },
+    {
+        name: 'Gmail-Automation',
+        imageUrl: pinit,
+        projectUrl: '',
+        githubUrl: 'https://github.com/k8pai/gmail-automation',
+        Components: [
+            {
+                Component: SiJavascript,
+                color: '#3178C6',
+            },
+            {
+                Component: SiNodedotjs,
+                color: '#339933',
+            },
+            {
+                Component: SiGmail,
+                color: '#EA4335',
+            },
+            {
+                Component: SiGoogle,
+                color: '#4285F4',
+            },
+            {
+                Component: SiAuth0,
+                color: '#EB5424',
+            },
+        ],
+    },
+    {
+        name: 'Nextauth-cli',
+        imageUrl: pinit,
+        projectUrl: 'https://www.npmjs.com/package/nextauth-cli',
+        githubUrl: 'https://github.com/k8pai/nextauth-cli',
+        Components: [
+            {
+                Component: SiTypescript,
+                color: '#3178C6',
+            },
+            {
+                Component: SiAuth0,
+                color: '#EB5424',
+            },
+        ],
+    },
+    {
+        name: 'Linkwrap',
+        imageUrl: linkwrap,
+        projectUrl: 'https://linkwrap.k8pai.dev',
+        githubUrl: 'https://github.com/k8pai/linkwrap',
+        Components: [
+            {
+                Component: SiNextdotjs,
+                color: '#ffffff',
+                darkColor: '#000000',
+            },
+            {
+                Component: SiTailwindcss,
+                color: '#06B6D4',
+            },
+            {
+                Component: SiMongodb,
+                color: '#47A248',
+            },
+            {
+                Component: SiPrisma,
+                color: '#2D3748',
+            },
+            {
+                Component: SiFramer,
+                color: '#0055FF',
+            },
+        ],
+    },
+    {
+        name: 'Tailwind Inputs',
+        imageUrl: tailwindInputs,
+        projectUrl: 'https://ti-docs-2.vercel.app',
+        Components: [
+            {
+                Component: SiNextdotjs,
+                color: '#ffffff',
+            },
+            {
+                Component: SiTailwindcss,
+                color: '#06B6D4',
+            },
+            {
+                Component: SiFramer,
+                color: '#0055FF',
+            },
+        ],
+    },
+    {
+        name: 'Todolist',
+        imageUrl: todolist,
+        projectUrl: 'https://todolist-k8pai.vercel.app',
+        githubUrl: 'https://github.com/k8pai/todolist',
+        Components: [
+            {
+                Component: SiNextdotjs,
+                color: '#ffffff',
+            },
+            {
+                Component: SiTailwindcss,
+                color: '#06B6D4',
+            },
+        ],
+    },
+    {
+        name: 'Pinit (Chrome Extention)',
+        imageUrl: pinit,
+        // projectUrl: 'https://todolist-k8pai.vercel.app',
+        githubUrl: 'https://github.com/k8pai/pinit',
+        Components: [
+            {
+                Component: SiHtml5,
+                color: '#ffffff',
+            },
+            {
+                Component: SiTailwindcss,
+                color: '#06B6D4',
+            },
+            {
+                Component: SiJavascript,
+                color: '#06B6D4',
+            },
+        ],
+    },
+    // {
+    // 	name: 'Portfolio',
+    // 	imageUrl: portfolio,
+    // 	projectUrl: 'https://k8pai.dev',
+    // 	githubUrl: 'https://github.com/k8pai/k8pai.dev',
+    // 	Components: [
+    // 		{
+    // 			Component: SiNextdotjs,
+    // 			color: '#ffffff',
+    // 			darkColor: '#000000',
+    // 		},
+    // 		{
+    // 			Component: SiTailwindcss,
+    // 			color: '#06B6D4',
+    // 		},
+    // 		{
+    // 			Component: SiMongodb,
+    // 			color: '#47A248',
+    // 		},
+    // 		{
+    // 			Component: SiPrisma,
+    // 			color: '#2D3748',
+    // 		},
+    // 		{
+    // 			Component: SiFramer,
+    // 			color: '#0055FF',
+    // 		},
+    // 	],
+    // },
+]
