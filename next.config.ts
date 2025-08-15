61616c7a5c7088542228c5beed6edd0e969b4a86
k8pai <thek8pai@gmail.com>
2025-08-15 18:14:36 +0530

    fix: config changes for new version of nextjs, and removed unused contentlayer config and it's package.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import createMDX from '@next/mdx'
+import type { NextConfig } from 'next'
+import rehypeStringify from 'rehype-stringify'
+import remarkGfm from 'remark-gfm'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
     images: {
         domains: ['github.com', 'avatars.githubusercontent.com'],
@@ -30,11 +34,18 @@ const nextConfig = {
             },
         ]
     },
+    experimental: {
+        mdxRs: true,
+    },
 }
 
 const withMDX = createMDX({
     // Add markdown plugins here, as desired
     extension: /\.(md|mdx)$/,
+    options: {
+        remarkPlugins: [remarkGfm, remarkParse, remarkRehype],
+        rehypePlugins: [rehypeStringify],
+    },
 })
 
 // Merge MDX config with Next.js config
